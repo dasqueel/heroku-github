7dcd5f3f6ec120ba55df2ebd3e50ee157ccbc502
Neil Barduson <barduson.neil@gmail.com>
2018-02-17 16:34:01 -0600

    ports

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'))
+app.listen(5000, () => console.log('Example app listening on port 3001!'))
